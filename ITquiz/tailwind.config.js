e36a84d9c0b59ca50cf73cd938128c8f308ec89c
Dipanshu Sandhaki <dipanshuroi6@gmail.com>
2025-03-12 15:35:50 +0530

    Update the code quiz manage

--- ITquiz/tailwind.config.js
+++ ITquiz/tailwind.config.js
@@ -10,6 +10,7 @@ export default {
         primary: "#788492",
         secondary: "#919AA7",
         button1: "#0B1A70",
+        color_green: "#09A006"
       },
       fontFamily:{
         poppins: ["Poppins", "sans-serif"],
